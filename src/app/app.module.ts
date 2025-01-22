e975db672a5e953b339604fbc105d1b329ff49a0
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-01-22 12:23:43 +0100

    Refresh app & dev container

---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+
+
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SideMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NzPageHeaderModule,
+    BrowserAnimationsModule,
+    NzBreadCrumbModule,
+    NzSpaceModule,
+    NzButtonModule,
+    NzDrawerModule,
+    NzTabsModule
+  ],
+  // exports:[
+  //   NzPageHeaderModule
+  // ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
