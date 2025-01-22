8047e5d074d2fd160f9b557144ce4adbe060f5ee
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-01-22 12:25:52 +0100

    Remove duplicate folder

--- portfolio/src/services/scripts/openapi-generator.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm \
-    -v ${PWD}:/local openapitools/openapi-generator-cli generate \
-    -i /local/openapi.json \
-    -g typescript-fetch \
-    -o /local/openapi-generator-out/typescript-fetch \
-    --additional-properties=useSingleRequestParameter=false,supportsES6=true,withInterfaces=true
